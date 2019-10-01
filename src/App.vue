721b2ac5e604da1aea7336043f22c53c341ae4b8
devoab <dwuthk@gmail.com>
2019-10-02 01:04:13 +0900

    todomvc 컴포넌트 분리까지만

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <section class="todoapp">
+      <Input />
+      <TodoList />
+      <TodoListFilter />
+    </section>
+    <Footer />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import Input from "@/components/Input";
+import Footer from "@/components/Footer";
+import TodoList from "@/components/TodoList";
+import TodoListFilter from "@/components/TodoListFilter"
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: "app",
+  components: {
+    Input,
+    Footer,
+    TodoList,
+    TodoListFilter
+  }
+};
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style src="../node_modules/todomvc-app-css/index.css" />
---
+++ src/components/Input.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input class="new-todo" placeholder="What needs to be done?" autofocus />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Input"
+};
+</script>
